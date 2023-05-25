9b930c96d7f6a81accb17a4e2568477fc5302d30
mohamed <mlamine642@gmail.com>
2023-05-25 21:11:03 +0100

    create typing app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import{ LoremIpsum} from 'lorem-ipsum'
+
+
+
+    const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+      max: 8,
+      min: 4
+    },
+    wordsPerSentence: {
+      max: 5,
+      min: 3
+    }
+  });
 
 @Component({
   selector: 'app-root',
@@ -6,5 +20,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+
+  text = lorem.generateSentences(1);
+  entredText='';
+
+  onInput(text :any){
+    this.entredText=text.value
+    
+  }
+
+  compare(randomLetter:string,entredLetter:string):string{
+    if(!entredLetter)
+    return 'pending';
+
+   return entredLetter===randomLetter?'correct':'incorrect'
+    
+
+  }
 }
